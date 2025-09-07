d0ac244253517278ab9bf18e17aed4999589ab5f
parapat-p <parapat.patc@gmail.com>
2025-09-07 12:47:04 +0700

    Add operate function for calculator

--- script.ts
+++ script.ts
@@ -13,3 +13,16 @@ function multiply(a:number,b:number){
 function divide(a:number,b:number){
     return a/b;
 }
+
+function operate(a:number,b:number,operator:string){
+    switch(operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
